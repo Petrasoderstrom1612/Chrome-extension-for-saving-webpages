c8bb3a7ea11ba35af46b751e8a23aa2a7c213f99
Petra Söderström <101672601+Petrasoderstrom1612@users.noreply.github.com>
2024-07-24 08:36:13 +0200

    forloop example

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
 const inputEl = document.getElementById("input-el")
 const ulEl = document.getElementById("ul-el")
-let mySites = []
+let mySites = ["kkk","lll"]
 
 
 document.getElementById("input-btn").addEventListener("click", function(){
     mySites.push(inputEl.value)
+    console.log(mySites)
 })
 
-for (let i = 0; i < myLeads.length; i++) {
-    console.log(myLeads[i])
-    ulEl.innerHTML += `<li>${myLeads[i]}</li>`
+for (let i = 0; i < mySites.length; i++) {
+    console.log(mySites[i])
+    ulEl.innerHTML += `<li>${mySites[i]}</li>`
 }
