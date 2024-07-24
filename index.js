09c8970a494e9ef79f1171e9478ff62bd1d6f182
Petra Söderström <101672601+Petrasoderstrom1612@users.noreply.github.com>
2024-07-24 10:39:00 +0200

    forloop and extra variable to store the loops in to avoid unnecessary rerendering

--- index.js
+++ index.js
@@ -1,14 +1,20 @@
 const inputEl = document.getElementById("input-el")
 const ulEl = document.getElementById("ul-el")
-let mySites = ["kkk","lll"]
+let mySites = ["kkk","lll","g"]
 
 
 document.getElementById("input-btn").addEventListener("click", function(){
     mySites.push(inputEl.value)
-    console.log(mySites)
+    inputEl.value = ""
+    renderLeads()
 })
 
-for (let i = 0; i < mySites.length; i++) {
-    console.log(mySites[i])
-    ulEl.innerHTML += `<li>${mySites[i]}</li>`
+
+function renderLeads(){ //instead of looping through mySites and slowing down the page, we can create one extra variable and keep adding the new mySites to it and only render the stored new variable with all mySites, instead of updating on every loop. Remember! DOM manipulation comes with a cost.
+    let listItems = ""
+    for (let i = 0; i < mySites.length; i++) {
+    listItems += `<li>${mySites[i]}</li>`
+    }
+    ulEl.innerHTML = listItems
 }
+
