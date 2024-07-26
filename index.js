3f20af022914b7cce20cf6103f8ab693c417446b
Petra Söderström <101672601+Petrasoderstrom1612@users.noreply.github.com>
2024-07-26 12:05:53 +0200

    saved to local storage and displaied

--- index.js
+++ index.js
@@ -2,16 +2,24 @@ const inputEl = document.getElementById("input-el")
 const ulEl = document.getElementById("ul-el")
 let mySites = []
 
-renderLeads()
+let mySitesFromLocalStorage = JSON.parse(localStorage.getItem("mySites")) //declare a variable my sites from local storage to be the parsed value from local storage
+console.log(mySitesFromLocalStorage)
+
+if(mySitesFromLocalStorage) { //if I have some sites saved in local storage, I want to display them on render
+    mySites = mySitesFromLocalStorage //give my array the values from local storage
+    displayMySites() //display it
+}
 
 document.getElementById("input-btn").addEventListener("click", function(){
-    mySites.push(inputEl.value)
-    inputEl.value = ""
-    renderLeads()
+    mySites.push(inputEl.value) //on user's input ass the users input to the array mySites
+    inputEl.value = ""  //clear the input field so it looks pretty and indicates you added your input 
+
+    localStorage.setItem("mySites", JSON.stringify(mySites)) //save the new input into local storage
+    displayMySites() //display all the inputs
 })
 
 
-function renderLeads(){ //instead of looping through mySites and slowing down the page, we can create one extra variable and keep adding the new mySites to it and only render the stored new variable with all mySites, instead of updating on every loop. Remember! DOM manipulation comes with a cost.
+function displayMySites(){ //instead of looping through mySites and slowing down the page, we can create one extra variable and keep adding the new mySites to it and only render the stored new variable with all mySites, instead of updating on every loop. Remember! DOM manipulation comes with a cost.
     let listItems = ""
     for (let i = 0; i < mySites.length; i++) {
     listItems += `<li><a href="${mySites[i]}" target="_blank">${mySites[i]}</a></li>`
