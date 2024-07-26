e2e993920ef9e46f9e365b7408218ba1f6ed9acf
Petra Söderström <101672601+Petrasoderstrom1612@users.noreply.github.com>
2024-07-26 14:55:34 +0200

    added a delete button with clearing function fot the specific variable, added comments, turned mySites into argument so that displayMySites function can be reused

--- index.js
+++ index.js
@@ -2,28 +2,34 @@ const inputEl = document.getElementById("input-el")
 const ulEl = document.getElementById("ul-el")
 let mySites = []
 
-let mySitesFromLocalStorage = JSON.parse(localStorage.getItem("mySites")) //declare a variable my sites from local storage to be the parsed value from local storage
+let mySitesFromLocalStorage = JSON.parse(localStorage.getItem("mySites")) //declare a variable my sites from local storage, to be the parsed value from local storage
 console.log(mySitesFromLocalStorage)
 
 if(mySitesFromLocalStorage) { //if I have some sites saved in local storage, I want to display them on render
     mySites = mySitesFromLocalStorage //give my array the values from local storage
-    displayMySites() //display it
+    displayMySites(mySites) //display it
 }
 
 document.getElementById("input-btn").addEventListener("click", function(){
-    mySites.push(inputEl.value) //on user's input ass the users input to the array mySites
-    inputEl.value = ""  //clear the input field so it looks pretty and indicates you added your input 
+    mySites.push(inputEl.value) //on user's input add the user's input to the array mySites
+    inputEl.value = ""  //clear the input field so it looks pretty and UI indicates you added your input 
 
     localStorage.setItem("mySites", JSON.stringify(mySites)) //save the new input into local storage
-    displayMySites() //display all the inputs
+    displayMySites(mySites) //display all the inputs
 })
 
 
-function displayMySites(){ //instead of looping through mySites and slowing down the page, we can create one extra variable and keep adding the new mySites to it and only render the stored new variable with all mySites, instead of updating on every loop. Remember! DOM manipulation comes with a cost.
+function displayMySites(desiredArrayOfSites){ //instead of looping through mySites and slowing down the page, we can create one extra variable and keep adding the new mySites to it and only render the stored new variable with all mySites, instead of updating on every loop. Remember! DOM manipulation comes with a cost.
     let listItems = ""
-    for (let i = 0; i < mySites.length; i++) {
-    listItems += `<li><a href="${mySites[i]}" target="_blank">${mySites[i]}</a></li>`
+    for (let i = 0; i < desiredArrayOfSites.length; i++) {
+    listItems += `<li><a href="${desiredArrayOfSites[i]}" target="_blank">${desiredArrayOfSites[i]}</a></li>`
     }
     ulEl.innerHTML = listItems
 }
 
+document.getElementById("delete-btn").addEventListener("click", function(){
+    ulEl.innerHTML = listItems = ""
+    mySitesFromLocalStorage = localStorage.clear()
+    mySites = []
+    console.log(mySites)
+})
